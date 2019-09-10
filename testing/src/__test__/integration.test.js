f2e0604a633bc38c6d841b30e87dbea2aefb2d97
rangga <ranggad990@gmail.com>
2019-09-10 22:42:32 +0700

    063-Fixing-a-Broken-Test

--- testing/src/__test__/integration.test.js
+++ testing/src/__test__/integration.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Root from 'Root';
-import App from 'component/App';
+import App from 'components/App';
 
 it('can fetch a list of comments and display them', () => {
     // Attempt to render the *entire* app
+    const wrapped = mount(
+      <Root>
+        <App />
+      </Root>
+    );
 
     // find the 'fetchComments' button and click it
 
--- testing/src/components/__test__/CommentBox.test.js
+++ testing/src/components/__test__/CommentBox.test.js
@@ -17,9 +17,9 @@ afterEach(() =>{
     wrapped.unmount();
 });
 
-it('has a text area and a button', () => {
+it('has a text area and two button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('button').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(2);
 });
 
 describe('the text area', () => {
@@ -33,7 +33,7 @@ describe('the text area', () => {
     it('has a text area that users can type in', () => {
         expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
     });
-    
+
     it('when form is submitted, text area gets emptied', () =>{
         wrapped.find('form').simulate('submit');
         wrapped.update();
