70b731d1dcbd30c19c6d676c921e7ed6775fd4a0
rangga <ranggad990@gmail.com>
2019-09-10 23:38:58 +0700

    069-Moxioss-Wait-Function

--- testing/src/__test__/integration.test.js
+++ testing/src/__test__/integration.test.js
@@ -28,12 +28,12 @@ it('can fetch a list of comments and display them', (done) => {
     wrapped.find('.fetch-comments').simulate('click');
 
     // Pause
-    setTimeout(() => {
+    moxios.wait(() => {
       wrapped.update();
       // Expect to find a list of comments!
       expect(wrapped.find('li').length).toEqual(2);
 
       done();
       wrapped.unmount();
-    }, 100);
+    });
 });
